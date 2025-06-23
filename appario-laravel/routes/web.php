cbc562af8fb7b509e078f7bc7358983a3c14c75d
Pedro Sousa <pedrohenriquemacielldesousa@gmail.com>
2025-06-23 20:48:58 -0300

    Resolvendo conflito em routes/web.php após merge.

--- appario-laravel/routes/web.php
+++ appario-laravel/routes/web.php
@@ -8,21 +8,14 @@ use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\Auth\ForgotPasswordController;
 use App\Http\Controllers\Auth\ResetPasswordController;
 
-<<<<<<< HEAD
 Route::get('/', function () {
     return view('homepage.homepage');
 });
 
-Route::get('/login', function () {
-    return view('usuarios.login');
-})->name('login.form');
-Route::post('/login', [AuthController::class, 'login'])->name('login');
-=======
 // ROTAS DE LOGIN E USUÁRIOS (fora de auth)
 Route::group([], function () {
     Route::get('/login', fn() => view('usuarios.login'))->name('login.form');
     Route::post('/login', [AuthController::class, 'login'])->name('login');
->>>>>>> ccaf4dc9633d34f28606c06ac77a8462b3c16b71
 
     // Redefinição de senha
     Route::get('/esqueci-senha', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('usuarios.solicitarSenha');
@@ -52,14 +45,3 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/apicultor', fn() => 'Página de apicultores em construção.')->name('apicultor.index');
    
 });
-
-
-/*Route::resource('pessoas', PessoaController::class);
-Route::get('/pessoas', [PessoaController::class, 'index'])->name('pessoas.listar');
-Route::post('/pessoas', [PessoaController::class, 'store'])->name('pessoas.inserir');
-Route::put('/pessoas/{pessoa}', [PessoaController::class, 'update'])->name('pessoas.update');
-Route::get('/pessoas/{pessoa}', [PessoaController::class, 'show'])->name('pessoas.show');
-Route::delete('/pessoas/{pessoa}', [PessoaController::class, 'destroy'])->name('pessoas.destroy');
-Route::get('/pessoas/create', [PessoaController::class, 'create'])->name('pessoas.create');
-Route::get('/pessoas/{pessoa}/edit', [PessoaController::class, 'edit'])->name('pessoas.edit');
-Route::get('/pessoas/{pessoa}/delete', [PessoaController::class, 'delete'])->name('pessoas.delete');*/
